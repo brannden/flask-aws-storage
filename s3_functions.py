0648dd840af009905d11c6a37368fd2f27060959
Brannden Evans <55254906+brannden@users.noreply.github.com>
2021-11-08 10:37:39 -0600

    Update s3_functions.py
    Added Line 3, updated line 23 to allow for newer AWS signature versions

--- s3_functions.py
+++ s3_functions.py
@@ -1,5 +1,7 @@
 import boto3
 
+from botocore.client import Config
+
 def upload_file(file_name, bucket):
     object_name = file_name
     s3_client = boto3.client('s3')
@@ -18,7 +20,7 @@ def list_files(bucket):
     return contents
 
 def show_image(bucket):
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3', config=Config(signature_version='s3v4'))
     # location = boto3.client('s3').get_bucket_location(Bucket=bucket)['LocationConstraint']
     public_urls = []
     try:
